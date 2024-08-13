ccc4912c5466d9c70d1d06e46d7984151211197c
iam-subhankardash <158249466+iam-subhankardash@users.noreply.github.com>
2024-08-13 22:06:56 +0530

    Add files via upload

---
+++ task01.py
@@ -0,0 +1,9 @@
+num1 = int(input("first number is :"))
+num2 = int(input("second number is :"))
+print(max(num1, num2))
+print(pow(num1, num2))
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
