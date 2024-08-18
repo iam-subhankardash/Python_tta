84dfad4e7223894be4fee08469ebb0ff2fd6cdc6
SUBHANKAR <isubhankar@gmail.com>
2024-08-18 09:09:18 +0530

    task

---
+++ 13082024/lab001.py
@@ -0,0 +1,32 @@
+# number = 3.14253356
+# formatted_num = f"{number:.3f}" #f => format
+# print(formatted_num)  #3.143
+
+#Task-1
+# table = float(input("The given number is:"))
+# print(f"{table}*1={table*1}")
+# print(f"{table}*2={table*2}")
+# print(f"{table}*3={table*3}")
+# print(f"{table}*4={table*4}")
+# print(f"{table}*5={table*5}")
+# print(f"{table}*6={table*6}")
+# print(f"{table}*7={table*7}")
+# print(f"{table}*8={table*8}")
+# print(f"{table}*9={table*9}")
+# print(f"{table}*10={table*10}")
+
+#Task-2
+num1 = int(input('The first number is:'))
+num2 = int(input('The second number is:'))
+result_sum = num1+num2
+result_sub = num1-num2
+result_pwr = pow(num1,num2)
+result_max = max(num1,num2)
+result_mul = num1*num2
+result_div = num1/num2
+print(f"The sum value is:",result_sum)
+print(f"The subtraction value is:",result_sub)
+print(f"The power num1 to num2:",result_pwr)
+print(f"The maximum value is:",result_max)
+print(f"The multiplication value is:",result_mul)
+print(f"The division value is:",result_div)
