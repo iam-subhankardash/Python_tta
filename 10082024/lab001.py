657d7d7e983a163102b6d79de52892e85ce9373b
SUBHANKAR <isubhankar@gmail.com>
2024-08-18 09:18:51 +0530

    class

---
+++ 10082024/lab001.py
@@ -0,0 +1,6 @@
+# source code -> human-readable code or code written by the humans - HLL
+# sep=' ' -> how you want to separate the arguments
+# end=' ' -> in the end what you want to do
+
+print('apple', 'orange', 'grapes', 'banana', 'fruits', sep=',', end='||')
+print('potato', 'tomato', 'carrot', sep=',')
---
+++ 10082024/lab002.py
@@ -0,0 +1,18 @@
+# keywords
+# import keyword
+# print(keyword.kwlist)
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def',
+# 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
+# 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+# variable -> Variable is container (storage area) used to hold data.
+# each variable should be given a unique name.(identifier)
+
+a = 10
+b = 20
+c = 30
+my_sum = a+b+c
+my_sum = my_sum-15
+my_sum = my_sum+5
+print(my_sum)
+
