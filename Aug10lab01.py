cf35912c0c72284016c9981a2779808069323f10
SUBHANKAR <isubhankar@gmail.com>
2024-08-20 20:31:27 +0530

    class

--- Aug10lab01.py
+++ Aug10lab01.py
@@ -2,5 +2,5 @@
 # sep=' ' -> how you want to separate the arguments
 # end=' ' -> in the end what you want to do
 
-print('apple', 'orange', 'grapes', 'banana', 'fruits', sep=',', end='||')
-print('potato', 'tomato', 'carrot', sep=',')
+print('apple', 'orange', 'grapes', 'banana', 'fruits', sep='|', end='==>')
+print('potato', 'tomato', 'carrot', sep='||')
